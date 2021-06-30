4fdc7b28b9428deda33fad229ca18756792f44b3
FOCUS <focus@Andrews-MBP.hitronhub.home>
2021-06-30 18:22:29 -0400

    navbar css

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,44 @@
-import Head from 'next/head'
+import Head from "next/head";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <div>
       <Head>
-        <h1> hello from the head </h1>
+        <nav className="bg-green-dark h-36">
+          <ul className="flex justify-between">
+            <li className="flex items-center">
+              <img
+                src="https://i.postimg.cc/85Cb11Nf/PSSO-Logo-White.png"
+                className="object-contain h-36"
+              ></img>
+              <div>
+                <h1 className="text-white text-2xl font-Poppins font-semibold">
+                  Public Schools Speech Organization
+                </h1>
+                <h3 className="text-white">
+                  Inspiring the next generation of great speakers
+                </h3>
+              </div>
+            </li>
+            <li className="flex items-center">
+              <div className="flex space-between">
+                <Link href="/">
+                  <a className="text-white  px-4">Home</a>
+                </Link>
+                <Link href="/">
+                  <a className="text-white px-4">About Us</a>
+                </Link>
+                <Link href="/">
+                  <a className="text-white px-4">Events and Programs</a>
+                </Link>
+                <Link href="/">
+                  <a className="text-white px-4">Contact</a>
+                </Link>
+              </div>
+            </li>
+          </ul>
+        </nav>
       </Head>
 
       <main>
@@ -15,5 +49,5 @@ export default function Home() {
         <h1> hello from the footer</h1>
       </footer>
     </div>
-  )
+  );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,23 @@
 module.exports = {
-  mode: 'jit',
-  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  mode: "jit",
+  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        green: {
+          primary: "#1c6e28",
+          dark: "#065721",
+          darker: "#094b1e",
+        },
+      },
+      fontFamily: {
+        Poppins: '"Poppins", Helvetica, Arial, sans-serif',
+      },
+    },
   },
   variants: {
     extend: {},
   },
   plugins: [],
-}
+};
