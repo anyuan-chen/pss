a7777acb391c6cb3365d2cb01e5024fdc546a8b6
Andrew Chen <andrew.chen.anyuan@gmail.com>
2021-07-01 13:58:44 -0400

    fixing react rendering deployment error

---
+++ pages/about.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import Navbar from '../components/navbar'
+export default function About() {
+    return (
+        <div>
+            <Navbar></Navbar>
+        </div>
+    )
+}
