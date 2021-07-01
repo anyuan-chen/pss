6f8acb703ed783d084e9600bca0285fbe5631f5e
Andrew Chen <andrew.chen.anyuan@gmail.com>
2021-07-01 13:54:04 -0400

    changes to box layout

--- components/box.js
+++ components/box.js
@@ -1,10 +1,13 @@
 import React from 'react'
-
+import Link from 'next/link'
 export default function Box(props) {
     return (
-        <div>
-            <h1>{props.title}</h1>
-            <p>{props.body}</p>
+        <div className="flex flex-col items-center h-96 bg-green-dark justify-around">
+            <h1 className="text-white font-bold text-3xl">{props.title}</h1>
+            <p className="text-white px-32">{props.body}</p>
+            <Link href="/aboutPublicSpeaking">
+                <a className="text-white underline">Learn More</a>
+            </Link>
         </div>
     )
 }
