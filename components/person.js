f3bd7d57ed932d6419186847d63c6b79df1d59dc
Andrew Chen <andrew.chen.anyuan@gmail.com>
2021-07-01 14:42:16 -0400

    about page

--- components/person.js
+++ components/person.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from "react";
 /*
     This is a person component designed for usage on the "meet our team section of the page"
     Example can be found in /pages/meetttheteam.js
@@ -9,11 +9,14 @@ import React from 'react'
         desc - description of person
 */
 export default function person(props) {
-    return (
-        <div>
-            <h1>{props.name}</h1>
-            <img href = {props.imgUrl}></img>
-            <p>{props.desc}</p>
-        </div>
-    )
+  return (
+    <div className="grid grid-cols-2">
+      <div className="flex items-center flex-col">
+        <h1 className="pt-10 pb-5 text-3xl">{props.name}</h1>
+        <h2 className="pb-8 text-xl">{props.title}</h2>
+        <p className="px-24 text-lg">{props.desc}</p>
+      </div>
+      <img src={props.imgUrl}></img>
+    </div>
+  );
 }
--- pages/meettheteam.js
+++ pages/meettheteam.js
@@ -1,12 +1,53 @@
-import React from 'react'
-import Navbar from '../components/navbar'
+import React from "react";
+import Navbar from "../components/navbar";
+import Person from "../components/person";
 export default function MeetTheTeam() {
-    return (
-        <div>
-            <Navbar></Navbar>
-            <div>
-                <h1>Meet Our Team</h1>
-            </div>
-        </div>
-    )
+  return (
+    <div>
+      <Navbar></Navbar>
+      <div>
+        <header className="flex justify-center text-6xl pt-8 pb-10">
+          <h1>Meet Our Team</h1>
+        </header>
+        <main>
+          <Person
+            title="President"
+            name="Christopher Lam"
+            imgUrl="https://i.imgur.com/4o64tK7.png"
+            desc="Hello, my name is Christopher Lam, and I’m thrilled to be a Co-President and a Co-Founder of Public Schools Speech Organization! As a high school student myself, I understand how competitive the academic landscape is becoming. At the same time, I also know that public speaking skills are the key to helping you stand out. Through PSSO, I hope to share my public speaking knowledge and experience with all of you to help you reach your full potential. Welcome to the PSSO community!"
+          ></Person>
+          <Person
+            title="Director of Competitions"
+            name="David Zhuang"
+            imgUrl=""
+            desc=""
+          ></Person>
+          <Person
+            title="Director of External Affairs"
+            name="Jia Jia He"
+            imgUrl=""
+            desc=""
+          ></Person>
+          <Person
+            title="Web Developer"
+            name="Andrew Chen"
+            imgUrl=""
+            desc=""
+          ></Person>
+          <Person
+            title="External Affairs Coordinator"
+            name="Ryan Yin"
+            imgUrl=""
+            desc=""
+          ></Person>
+          <Person
+            title="Secretary"
+            name="Hailey Lyn"
+            imgUrl=""
+            desc=""
+          ></Person>
+        </main>
+      </div>
+    </div>
+  );
 }
